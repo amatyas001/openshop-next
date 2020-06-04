49dd2749fe71658abf61f457c7da7862f8ad6301
Mátyás Angyal <amatyas001@gmail.com>
2020-06-05 00:11:09 +0200

    feature(styles): cleanup

---
+++ styles/theme.js
@@ -0,0 +1,10 @@
+import { theme } from '@chakra-ui/core';
+
+export default {
+  ...theme,
+  fonts: {
+    heading: 'Khand, sans-serif',
+    body: 'Montserrat, sans-serif',
+    mono: 'Menlo, monospace',
+  },
+};
